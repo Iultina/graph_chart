bef3c19e3fddc9054306f84f7c30d75633b0e5f5
Iultina <117123418+Iultina@users.noreply.github.com>
2023-08-15 22:35:12 +0000

    Modify models

--- charts/models.py
+++ charts/models.py
@@ -7,26 +7,44 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+
 class Year(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     year = models.IntegerField()
 
     def __str__(self):
-        return f"{self.city.name} - {self.year}"
+        return f'{self.city.name} - {self.year}'
     
     class Meta:
         unique_together = ('city', 'year')
 
+
 class Plan(models.Model):
     year = models.ForeignKey(Year, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Planned Sales')
 
     def __str__(self):
-        return f"{self.year} - Plan: {self.amount}"
-    
+        return f'{self.year} - Plan: {self.amount}'
+
+
 class Fact(models.Model):
     year = models.ForeignKey(Year, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Actual Sales')
 
     def __str__(self):
         return f"{self.year} - Fact: {self.amount}"
+
+
+# class Sales(models.Model):
+#       """Модель Sales вместо трех моделей Year, Plan, Fact.
+#       """
+#     city = models.ForeignKey(City, on_delete=models.CASCADE)
+#     year = models.IntegerField()
+#     plan_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Planned Sales')
+#     fact_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Actual Sales')
+
+#     def str(self):
+#         return f'{self.city.name} - {self.year}'
+
+#     class Meta:
+#         unique_together = ('city', 'year')
